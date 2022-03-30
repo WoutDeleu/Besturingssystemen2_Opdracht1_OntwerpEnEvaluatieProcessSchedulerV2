3ecdfe88ff2a4626afa8c80e391d599fb3596584
WoutDeleu <80529253+WoutDeleu@users.noreply.github.com>
2022-03-30 18:42:15 +0200

    Grafieken fixen

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -39,9 +39,9 @@ public class Main {
 
 
         //1.FCFS
-/*
+
         FCFS fcfs = new FCFS();
-        List<Process> fcfs_res = new ArrayList<>(fcfs.schedule(test));
+        List<Process> fcfs_res = new ArrayList<>(fcfs.schedule(processes3));
 
         Collections.sort(fcfs_res, new ServiceTimeComparator());
         makeClusters(cluster, fcfs_res);
@@ -105,10 +105,10 @@ public class Main {
 
         addToDataset(cluster, dataset_wait, dataset_tat, "RR (q=2)");
         for(Process p : processes3) p.reset();
-*/
+
         //5. RR (q=4)
         RR rr4= new RR(4);
-        List<Process> rr_res4 = new ArrayList<>(rr4.schedule(test));
+        List<Process> rr_res4 = new ArrayList<>(rr4.schedule(processes3));
 
         Collections.sort(rr_res4, new ServiceTimeComparator());
         makeClusters(cluster, rr_res4);
@@ -121,11 +121,11 @@ public class Main {
         addToDataset(cluster, dataset_wait, dataset_tat, "RR (q=4)");
         for(Process p : test) p.reset();
 
-/*
+
         //6. RR (q=8)
 
-        RR rr8 = new RR(10000);
-        List<Process> rr_res8 = new ArrayList<>(rr8.schedule(test));
+        RR rr8 = new RR(8);
+        List<Process> rr_res8 = new ArrayList<>(rr8.schedule(processes3));
 
         Collections.sort(rr_res8, new ServiceTimeComparator());
         makeClusters(cluster, rr_res8);
@@ -191,7 +191,7 @@ public class Main {
         addToDataset(cluster, dataset_wait, dataset_tat, "MLFB2");
         for(Process p : processes3) p.reset();
 
-        */
+
         // Plotten
         plot( "Gen. Tat", dataset_tat );
         plot( "Waittime", dataset_wait );
